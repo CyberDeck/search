312db375907ef9b9abacafab6a3217da480f3f0a
H. Gregor Molter <gregor.molter@secretlab.de>
2018-03-24 17:22:06 +0100

    Fixed Fixtures

--- test/search/dsl_test.rb
+++ test/search/dsl_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Search::Dsl::Test < ActiveSupport::TestCase
   test "truth" do
+    assert_kind_of Module, Search
     assert_kind_of Module, Search::Dsl
-    binding.pry
   end
 end
